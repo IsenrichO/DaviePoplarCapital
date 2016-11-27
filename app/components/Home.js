890a742ac222043b1a5cbdf0a4d40f410b0481b7
Oliver Isenrich <IsenrichO@gmail.com>
2016-11-27 14:14:39 -0800

    refactor: Remove nested <main> tag

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,25 +2,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-import DPCLogo from './constants/svg/DPCLogo_SVG';
+import DPCLogo from '../constants/svg/DPCLogo_SVG';
 import Footer from './Footer';
 
 
 const Home = () => (
-  <div>
-    <main>
-      <div id="centerFill">
-        <Link to="about">
-          <DPCLogo />
-        </Link>
-      </div>
-    </main>
-    <Footer />
+  <div id="centerFill">
+    <Link to="about">
+      <DPCLogo />
+    </Link>
   </div>
 );
 
 export default Home;
-
-
-// <a href="About.html">
-// </a>
