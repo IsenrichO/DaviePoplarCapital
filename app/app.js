022b90d6438b74583c0a34a69d5f17b438cabb0d
Oliver Isenrich <IsenrichO@gmail.com>
2016-11-26 19:40:25 -0800

    chore: Import react-router named exports

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import { Router, Route, hashHistory, browserHistory } from 'react-router';
 import NavBar from './NavBar';
 import CenteredLogo from './CenteredLogo';
 
