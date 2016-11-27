6aa1c6ee9d0310cd2ee62c5e1df9b915eecb2393
Oliver Isenrich <IsenrichO@gmail.com>
2016-11-26 18:53:36 -0800

    feat: Add onClick event-handler

--- app/NavBar.js
+++ app/NavBar.js
@@ -1,13 +1,46 @@
 'use strict';
 import React, { Component } from 'react';
 
+// Control of sidebar navigation menu display and behavior:
+const toggleNav = () => {
+  let nav = document.getElementById('nav-links-wrapper'),
+      glyph = document.getElementById('hamburger-icon');
+
+  // Function 'active' induces active sidebar menu display state:
+  function active() {
+    nav.className = 'active';
+    // nav.style.display = 'block';
+    glyph.style.color = 'White';
+    glyph.style.transform = 'rotate(90deg)';
+  }
+
+  // Function 'inactive' induces inactive sidebar menu display state:
+  function inactive() {
+    nav.className = 'inactive';
+    // nav.style.display = 'none';
+    glyph.style.color = 'rgb(79, 152, 201)';
+    glyph.style.transform = 'rotate(0deg)';
+  }
+
+  // Ternary conditional to change or revert based on present state of navigation menu sidebar:
+  nav.style.display !== 'block' ? active() : inactive();
+
+  // Event listener controls automated closure of navigation menu sidebar on outside click event:
+  window.addEventListener('mouseup', function(evt) {
+    if (evt.target != nav && evt.target.parentNode != nav && evt.target !== glyph) {
+      inactive();
+    }
+  });
+};
+
+
 const NavBar = () => (
   <div id="nav-bar">
     <span
-      id="dropDown_Btn"
-      // onclick="toggleNav()"
+      id="hamburger-icon"
+      onClick={ () => toggleNav() }
       data-charRef="HTML Character, Heaven Trigram (a.k.a. the 'hamburger' icon): ☰">&#9776;</span>
-    <div id="navBar_Cont">
+    <div id="nav-links-wrapper">
       <nav
         id="inter-pageNavBar"
         data-styleRef="color: White; text-shadow: 0 0 5px White;">
