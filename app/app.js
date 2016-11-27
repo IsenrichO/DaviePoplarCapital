3d99bf9a451b58864ccc4b7a402a8d955140895a
Oliver Isenrich <IsenrichO@gmail.com>
2016-11-26 19:29:44 -0800

    chore: Import DPCLogo component

--- app/app.js
+++ app/app.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import NavBar from './NavBar';
+import DPCLogo from './DPCLogo_SVG';
 
 // Top-level React (pure functional) component:
 const App = () => (
   <div>
     <NavBar />
+    <DPCLogo />
   </div>
 );
 
