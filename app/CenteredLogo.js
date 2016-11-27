fce8f1ea5b6f7322280a43a83d2c26ec3258f914
Oliver Isenrich <IsenrichO@gmail.com>
2016-11-26 19:32:05 -0800

    feat: Wrap DPC SVG in anchor tag

---
+++ app/CenteredLogo.js
@@ -0,0 +1,13 @@
+'use strict';
+import React, { Component } from 'react';
+import DPCLogo from './DPCLogo_SVG';
+
+const CenteredLogo = () => (
+  <div id="centerFill">
+    <a href="About.html">
+      <DPCLogo />
+    </a>
+  </div>
+);
+
+export default CenteredLogo;
