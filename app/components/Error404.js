568f9b8f8fd2989c89c71802295f6de5268bdb83
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-14 11:05:19 -0800

    chore: Assign appropriate className

--- app/components/Error404.js
+++ app/components/Error404.js
@@ -3,12 +3,12 @@ import React from 'react';
 
 
 const Error404 = () => (
-  <div id='error-wrapper'>
+  <div className="error-wrapper">
     <img
       src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJAAAACQAQMAAADdiHD7AAAABlBMVEUAAABTU1OoaSf/AAAAAXRSTlMAQObYZgAAAFJJREFUeF7t0cENgDAMQ9FwYgxG6WjpaIzCCAxQxVggFuDiCvlLOeRdHR9yzjncHVoq3npu+wQUrUuJHylSTmBaespJyJQoObUeyxDQb3bEm5Au81c0pSCD8HYAAAAASUVORK5CYII="
       alt="Error 404 Frowny Face Image Blob" />
     <br /><hr /><br />
-    <h2>404.. This page is not found!</h2>
+    <h2>404... This page is not found!</h2>
   </div>
 );
 
