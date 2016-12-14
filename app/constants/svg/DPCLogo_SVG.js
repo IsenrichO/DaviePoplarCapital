eb84f0ab44223470609bc2e0e5faf95dbd80cda0
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-14 13:01:53 -0800

    chore: Remove all inline styles and delegate to the _svg Sass partial

--- app/constants/svg/DPCLogo_SVG.js
+++ app/constants/svg/DPCLogo_SVG.js
@@ -4,49 +4,31 @@ import React from 'react';
 
 const DPCLogo = () => (
   <svg
-    width="570px"
-    height="275px"
-    style={{
-      background: 'rgba(79, 152, 201, 0.75)',
-      letterSpacing: '3px'
-    }}>
+    className="DPC-logo"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg">
     <text
+      data-section="logo-head"
       x="50%"
-      y="52.5%"
-      textAnchor="middle"
-      style={{
-        font: 'bold 100px/110px Georgia, sans-serif',
-        fill: '#FFF',
-        textAlign: 'center'
-      }}>DPC</text>
+      y="52.5%">
+      DPC
+    </text>
     <text
+      data-section="logo-sub"
       x="50%"
-      y="72.5%"
-      textAnchor="middle"
-      fill="#FFF"
-      style={{
-        font: 'small-caps lighter 35px/35px Georgia, sans-serif',
-        stroke: 'none',
-        textAlign: 'center'
-      }}>Davie Poplar Capital</text>
+      y="72.5%">
+      Davie Poplar Capital
+    </text>
     <line
       x1="135px"
       y1="60%"
       x2="435px"
-      y2="60%"
-      style={{
-        stroke: '#FFF',
-        strokeWidth: '1'
-      }} />
+      y2="60%" />
     <line
       x1="135px"
       y1="77.5%"
       x2="435px"
-      y2="77.5%"
-      style={{
-        stroke: '#FFF',
-        strokeWidth: '1'
-      }} />
+      y2="77.5%" />
   </svg>
 );
 
