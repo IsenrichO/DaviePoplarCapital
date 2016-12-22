478c92671329d9acb912497d605799799a76174f
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-22 18:12:48 -0500

    feat: Build out helpful support content for 404 errors

--- app/components/Error404.jsx
+++ app/components/Error404.jsx
@@ -1,18 +1,21 @@
 'use strict';
 import React from 'react';
+import { Link } from 'react-router';
+
+import ErrorFace from '../../public/images/error_face.svg';
 
 
 const Error404 = () => (
   <div className="error-wrapper">
     <img
-      src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJAAAACQAQMAAADdiHD7AAAABlBMVEUAAABTU1OoaSf/AAAAAXRSTlMAQObYZgAAAFJJREFUeF7t0cENgDAMQ9FwYgxG6WjpaIzCCAxQxVggFuDiCvlLOeRdHR9yzjncHVoq3npu+wQUrUuJHylSTmBaespJyJQoObUeyxDQb3bEm5Au81c0pSCD8HYAAAAASUVORK5CYII="
-      alt="Error 404 Frowny Face Image Blob" />
-    <br /><hr /><br />
-    <h2>404... This page is not found!</h2>
+      src={ ErrorFace }
+      alt="Error 404 Frowny Face Image Blob (Gray)." />
+    <div>
+      <h3>404 Error... Looks like this page can't be found right now!</h3>
+      <hr />
+      <p>In the meantime, you can try <Link to="/">going back home</Link> to find what you need, or you can <a href="mailto:isenrich@yahoo.com">drop us a line</a> and we'll see if we can help resolve this issue.</p>
+    </div>
   </div>
 );
 
 export default Error404;
-
-
-// src2="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEgAAABIAQMAAABvIyEEAAAABlBMVEUAAABTU1OoaSf/AAAAAXRSTlMAQObYZgAAAENJREFUeF7tzbEJACEQRNGBLeAasBCza2lLEGx0CxFGG9hBMDDxRy/72O9FMnIFapGylsu1fgoBdkXfUHLrQgdfrlJN1BdYBjQQm3UAAAAASUVORK5CYII="
