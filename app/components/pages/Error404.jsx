a8615f63f5b6abae8ddbc1de9bb88ab5b0039f4c
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-26 23:16:13 -0800

    chore: Render content using jsx array expression

--- app/components/pages/Error404.jsx
+++ app/components/pages/Error404.jsx
@@ -13,7 +13,23 @@ const Error404 = () => (
     <div>
       <h3>404 Error... Looks like this page can't be found at this time!</h3>
       <hr />
-      <p>In the meantime, you can try <Link to="/" rel="prev">going back home</Link> to find what you need, or you can <a href="mailto:isenrich@yahoo.com" rel="help">drop us a line</a> and we'll see if we can help resolve this issue.</p>
+      <p>
+        {[
+          'In the meantime, you can try ',
+          <Link
+            to="/"
+            rel="prev">
+            going back home
+          </Link>,
+          ' to find what you need, or you can ',
+          <a
+            href="mailto:isenrich@yahoo.com"
+            rel="help">
+            drop us a line
+          </a>,
+          ' and we\'ll see if we can help resolve this issue.'
+        ]}
+      </p>
     </div>
   </div>
 );
