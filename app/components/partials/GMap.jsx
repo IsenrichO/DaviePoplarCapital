ffc634430e01ee1e05de56ea3f8ef06bb9db2b9f
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-26 23:33:04 -0800

    feat: Define map marker anchor and drop animation

--- app/components/partials/GMap.jsx
+++ app/components/partials/GMap.jsx
@@ -12,26 +12,33 @@ export default class GMap extends Component {
     super(props);
     this.addMarker = this.addMarker.bind(this);
     this.handleInfoWindowDisplayState = this.handleInfoWindowDisplayState.bind(this);
+    this.handleMarkerAnimation = this.handleMarkerAnimation.bind(this);
+    this.handleMarkerClick = this.handleMarkerClick.bind(this);
     this.renderInfoWindow = this.renderInfoWindow.bind(this);
     this.state = {
-      DPC_Coords: new google.maps.LatLng({ lat: 35.876172, lng: -78.641958 }),
-      infoWindowActive: false
+      DPC_Coords: new google.maps.LatLng({ lat: 35.875751, lng: -78.642080 }),
+      infoWindowActive: false,
+      markerAnimation: google.maps.Animation.DROP
     };
   }
 
   // Produces custom-styled map marker pin atop DPC's geographical coordinates:
   addMarker() {
-    const toggleInfoWindow = () => this.handleInfoWindowDisplayState();
     return (
       <Marker
+        ref="marker"
         position={ this.state.DPC_Coords }
+        animation={ this.state.markerAnimation }
         icon={{
           path: 'M61.5,0 C91,0 125,24.5 125,62.5 C125,94.5181999 78,185.5 61.5,185.5 C45,185.5 0,102.807291 0,62.5 C0,25 32,0 61.5,0 Z M63,96 C81.7776815,96 97,80.7776815 97,62 C97,43.2223185 81.7776815,28 63,28 C44.2223185,28 29,43.2223185 29,62 C29,80.7776815 44.2223185,96 63,96 Z',
           scale: 1 / 4,
+          size: new google.maps.Size(1550, 10),
+          anchor: new google.maps.Point(0, 130),
           fillColor: '#000',
           fillOpacity: 1
         }}
-        onClick={ toggleInfoWindow }>
+        onClick={ () => this.handleMarkerClick() }
+        >
         { this.state.infoWindowActive ? this.renderInfoWindow() : null }
       </Marker>
     );
@@ -40,6 +47,16 @@ export default class GMap extends Component {
   // Deactivates the display state of the clicked-upon map marker item's InfoWindow:
   handleInfoWindowDisplayState(bool) {
     this.setState({ infoWindowActive: bool !== undefined ? bool : !this.state.infoWindowActive });
+  }
+
+  handleMarkerAnimation() {
+    // this.state.infoWindowActive ? marker.setAnimation(google.maps.Animation.BOUNCE) : marker.setAnimation(null);
+    this.infoWindowActive ? this.setState({ markerAnimation: google.maps.Animation.BOUNCE }) : this.setState({ markerAnimation: null });
+  }
+
+  handleMarkerClick() {
+    this.handleInfoWindowDisplayState();
+    this.handleMarkerAnimation();
   }
 
   // Controls the content of map marker items' InfoWindow dialogue boxes:
@@ -61,6 +78,7 @@ export default class GMap extends Component {
         containerElement={ <div id="maps-container" /> }   
         googleMapElement={
           <GoogleMap 
+            ref="map"
             defaultCenter={ this.state.DPC_Coords }
             defaultZoom={ 14 }
             defaultOptions={{
