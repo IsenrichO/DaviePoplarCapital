0cac934a24c5e5c03962240299ed5dd8bf3f9ba2
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-28 15:08:35 -0800

    feat: Condense state and use default operator

--- app/components/partials/GMap.jsx
+++ app/components/partials/GMap.jsx
@@ -7,18 +7,17 @@ import LocationInfo from '../../constants/json/LocationInfoData.json';
 import { renderContactText } from '../../Mixins';
 
 
+// const GMap = () => {
 export default class GMap extends Component {
   constructor(props) {
     super(props);
     this.addMarker = this.addMarker.bind(this);
     this.handleInfoWindowDisplayState = this.handleInfoWindowDisplayState.bind(this);
-    this.handleMarkerAnimation = this.handleMarkerAnimation.bind(this);
-    this.handleMarkerClick = this.handleMarkerClick.bind(this);
     this.renderInfoWindow = this.renderInfoWindow.bind(this);
     this.state = {
-      DPC_Coords: new google.maps.LatLng({ lat: 35.875751, lng: -78.642080 }),
-      infoWindowActive: false,
-      markerAnimation: google.maps.Animation.DROP
+      defaultCenterCoords: new google.maps.LatLng({ lat: 35.879215, lng: -78.642146 }),
+      DPCCoords: new google.maps.LatLng({ lat: 35.875751, lng: -78.642080 }),
+      infoWindowActive: false
     };
   }
 
@@ -27,8 +26,8 @@ export default class GMap extends Component {
     return (
       <Marker
         ref="marker"
-        position={ this.state.DPC_Coords }
-        animation={ this.state.markerAnimation }
+        position={ this.state.DPCCoords }
+        animation={ google.maps.Animation.DROP }
         icon={{
           path: 'M61.5,0 C91,0 125,24.5 125,62.5 C125,94.5181999 78,185.5 61.5,185.5 C45,185.5 0,102.807291 0,62.5 C0,25 32,0 61.5,0 Z M63,96 C81.7776815,96 97,80.7776815 97,62 C97,43.2223185 81.7776815,28 63,28 C44.2223185,28 29,43.2223185 29,62 C29,80.7776815 44.2223185,96 63,96 Z',
           scale: 1 / 4,
@@ -37,8 +36,7 @@ export default class GMap extends Component {
           fillColor: '#000',
           fillOpacity: 1
         }}
-        onClick={ () => this.handleMarkerClick() }
-        >
+        onClick={ () => this.handleInfoWindowDisplayState() }>
         { this.state.infoWindowActive ? this.renderInfoWindow() : null }
       </Marker>
     );
@@ -46,17 +44,7 @@ export default class GMap extends Component {
 
   // Deactivates the display state of the clicked-upon map marker item's InfoWindow:
   handleInfoWindowDisplayState(bool) {
-    this.setState({ infoWindowActive: bool !== undefined ? bool : !this.state.infoWindowActive });
-  }
-
-  handleMarkerAnimation() {
-    // this.state.infoWindowActive ? marker.setAnimation(google.maps.Animation.BOUNCE) : marker.setAnimation(null);
-    this.infoWindowActive ? this.setState({ markerAnimation: google.maps.Animation.BOUNCE }) : this.setState({ markerAnimation: null });
-  }
-
-  handleMarkerClick() {
-    this.handleInfoWindowDisplayState();
-    this.handleMarkerAnimation();
+    this.setState({ infoWindowActive: bool || !this.state.infoWindowActive });
   }
 
   // Controls the content of map marker items' InfoWindow dialogue boxes:
@@ -78,8 +66,7 @@ export default class GMap extends Component {
         containerElement={ <div id="maps-container" /> }   
         googleMapElement={
           <GoogleMap 
-            ref="map"
-            defaultCenter={ this.state.DPC_Coords }
+            defaultCenter={ this.state.defaultCenterCoords }
             defaultZoom={ 14 }
             defaultOptions={{
               styles: GoogleMapStyles,
