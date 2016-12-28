6fbb443bcdb6c8b0ea52270eed0b2e33a21b6531
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-28 15:02:19 -0800

    feat: Wrap children props in ReactCSSTransitionGroup tag

--- app/components/MainLayout.jsx
+++ app/components/MainLayout.jsx
@@ -1,5 +1,6 @@
 'use strict';
-import React, { Component } from 'react';
+import React, { Component, cloneElement } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import NavBar from './partials/NavBar';
 import Footer from './partials/Footer';
@@ -7,22 +8,38 @@ import { assignClassNames } from '../Mixins';
 
 
 export default class MainLayout extends Component {
-  constructor(props) {
-    super(props);
-    this.getHashAsClass = this.getHashAsClass.bind(this);
-  }
-
-  componentDidMount() { this.getHashAsClass(); }
-  componentWillReceiveProps() { this.getHashAsClass(); }
-  getHashAsClass() { assignClassNames(this.refs.main); }
+  constructor(props) { super(props); }
+  componentDidMount() { assignClassNames(this.refs.transitionWrapper); }
+  componentWillReceiveProps() { assignClassNames(this.refs.transitionWrapper); }
 
   render() {
     return (
       <div className="main-layout">
         <NavBar />
-        <main ref="main">{ this.props.children }</main>
+          <div
+            id="transition-wrapper"
+            ref="transitionWrapper">
+            <ReactCSSTransitionGroup
+              component="main"
+              transitionName="router-transition-group"
+              transitionEnterTimeout={ 750 }
+              transitionLeaveTimeout={ 750 }>
+              {
+                cloneElement(this.props.children, {
+                  key: this.props.location.pathname
+                })
+              }
+            </ReactCSSTransitionGroup>
+          </div>
         <Footer />
       </div>
     );
   }
+};
+
+
+// Type Checking:
+MainLayout.propTypes = {
+  children: React.PropTypes.object,
+  location: React.PropTypes.object
 };
